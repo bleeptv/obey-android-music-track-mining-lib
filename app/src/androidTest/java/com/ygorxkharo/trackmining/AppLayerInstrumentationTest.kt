22a535ad5a49534dcbced2af0ccd2b628a797633
Robert Anizoba <robert@bleeptv.co>
2021-04-26 15:35:05 +0100

    B2MVE-1143 - Revert androidTests to JUnit 4

--- app/src/androidTest/java/com/ygorxkharo/trackmining/AppLayerInstrumentationTest.kt
+++ app/src/androidTest/java/com/ygorxkharo/trackmining/AppLayerInstrumentationTest.kt
@@ -2,16 +2,16 @@ package com.ygorxkharo.trackmining
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertTrue
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class AppLayerInstrumentationTest {
+class AppLayerInstrumentationTest {
 
     @Test
     fun testingWithAndroidEnv() {
         val app = InstrumentationRegistry.getInstrumentation().targetContext.applicationContext
-        assertTrue(app.applicationContext == null)
+        assertTrue(app.applicationContext != null)
     }
 }
--- data/src/androidTest/java/com/ygorxkharo/trackmining/DataLayerClassInstrumentationTest.kt
+++ data/src/androidTest/java/com/ygorxkharo/trackmining/DataLayerClassInstrumentationTest.kt
@@ -2,12 +2,12 @@ package com.ygorxkharo.trackmining
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.Test
+import org.junit.Assert.assertTrue
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class DataLayerClassInstrumentationTest {
+class DataLayerClassInstrumentationTest {
 
     @Test
     fun testingWithAndroidEnv() {
