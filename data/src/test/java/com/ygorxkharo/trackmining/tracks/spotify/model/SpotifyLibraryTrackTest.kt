8641ac0c9532f3a8ff21f46442d491497e11fb1e
Robert Anizoba <robert@bleeptv.co>
2021-04-28 18:03:40 +0100

    B2MVE-1146 - Write unit test for serialization/deserialization of Spotify track JSON response

---
+++ data/src/test/java/com/ygorxkharo/trackmining/tracks/spotify/model/SpotifyLibraryTrackTest.kt
@@ -0,0 +1,38 @@
+package com.ygorxkharo.trackmining.tracks.spotify.model
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.ygorxkharo.filesystem.TestFileUtils
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import com.ygorxkharo.trackmining.fixtures.spotify.SpotifyTrackJSONResultTestFixture
+
+internal class SpotifyLibraryTrackTest {
+
+    private val responsePayloadFileLocation = SpotifyTrackJSONResultTestFixture.responsePayloadFileLocation
+    private val moshiInstance = Moshi
+            .Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    private val likedSongsPayloadAdapter: JsonAdapter<SpotifyLikedSongsResult> = moshiInstance.adapter(SpotifyLikedSongsResult::class.java)
+
+
+    @Test
+    fun `test when transforming liked songs payload to JSON, expect output JSON string value to be the same`() {
+        // Arrange
+        val likedSongsPayloadJson = TestFileUtils.convertFileContentsToString(responsePayloadFileLocation)
+        val responsePayloadToJson = likedSongsPayloadAdapter.fromJson(likedSongsPayloadJson)
+        val expectedJsonString = likedSongsPayloadAdapter.toJson(responsePayloadToJson)
+
+        val spotifyLikeSongsResultObject = SpotifyLikedSongsResult(
+            trackItems = listOf(SpotifyTrackJSONResultTestFixture.buildSpotifyTrackItem())
+        )
+
+        //Act
+        val actualJsonString = likedSongsPayloadAdapter.toJson(spotifyLikeSongsResultObject)
+
+        //Assert
+        assertEquals(expectedJsonString, actualJsonString)
+    }
+}
