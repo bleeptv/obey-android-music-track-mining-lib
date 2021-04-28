df3a42ea7e9ecf16462c0fa2f36f362014e0e81e
Robert Anizoba <robert@bleeptv.co>
2021-04-28 18:17:16 +0100

    B2MVE-1148 - Add KDocs for file reader utility class

--- data/src/test/java/com/ygorxkharo/filesystem/TestFileUtils.kt
+++ data/src/test/java/com/ygorxkharo/filesystem/TestFileUtils.kt
@@ -4,9 +4,19 @@ import java.io.BufferedReader
 import java.io.InputStream
 import java.lang.StringBuilder
 
+/**
+ * Handles the loading/persistence of text files to use in a test environment
+ */
 object TestFileUtils {
 
+    /**
+     * Converts a file's contents into a string for further processing
+     *
+     * @param filePath File location in the filesystem
+     * @return the contents of the file in the form af a [String]
+     */
     fun convertFileContentsToString(filePath: String): String {
+
         val fileInputStream =  this.javaClass.classLoader?.getResourceAsStream(filePath)
         val bufferedReader = BufferedReader((fileInputStream as InputStream).reader())
         val content = StringBuilder()
