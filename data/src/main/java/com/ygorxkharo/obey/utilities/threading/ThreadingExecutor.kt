926dbd8648069d3f71cc91375ff5fa37dc747c71
Robert Anizoba <robert@bleeptv.co>
2021-04-29 17:35:54 +0100

    B2MVE-1149 - Rename method to perform tasks in the main application process

--- data/src/main/java/com/ygorxkharo/obey/utilities/threading/ThreadingExecutor.kt
+++ data/src/main/java/com/ygorxkharo/obey/utilities/threading/ThreadingExecutor.kt
@@ -13,21 +13,21 @@ object ThreadingExecutor {
     /**
      *
      * @param performInBackground Runs an operation on the background process
-     * @param onTaskCompleted Runs an operation on the foreground/main application process
+     * @param performInMainProcess Runs an operation on the foreground/main application process
      * @param contextProvider Provides the appropriate coroutine context for the background/foreground
      * operations of a task
      * @return a [Job] which will be executed
      */
     fun <T : Any> executeAsyncOperation(
         performInBackground: suspend (() -> T),
-        onTaskCompleted: ((T) -> Unit),
+        performInMainProcess: ((T) -> Unit),
         contextProvider: CoroutineContextProvider
     ): Job {
         return CoroutineScope(contextProvider.mainContext).launch {
             val resultFromBackgroundTask = CoroutineScope(contextProvider.ioContext).async IOScope@{
                 return@IOScope performInBackground()
             }.await()
-            onTaskCompleted(resultFromBackgroundTask)
+            performInMainProcess(resultFromBackgroundTask)
         }
     }
 }
