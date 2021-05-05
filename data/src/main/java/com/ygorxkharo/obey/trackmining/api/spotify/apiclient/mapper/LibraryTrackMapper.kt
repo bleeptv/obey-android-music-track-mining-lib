a6aca0b4443eecc07ba9287ccff265fd37a3d945
Robert Anizoba <robert@bleeptv.co>
2021-05-05 09:40:17 +0100

    B2MVE-1156 - Create library track mapper to convert a Spotify track to a domain model track

---
+++ data/src/main/java/com/ygorxkharo/obey/trackmining/api/spotify/apiclient/mapper/LibraryTrackMapper.kt
@@ -0,0 +1,141 @@
+package com.ygorxkharo.obey.trackmining.api.spotify.apiclient.mapper
+
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import com.ygorxkharo.obey.trackmining.api.spotify.apiclient.model.LibraryTrackEntity
+import com.ygorxkharo.trackmining.tracks.model.AlbumSource
+import com.ygorxkharo.trackmining.tracks.model.LibraryTrack
+import com.ygorxkharo.trackmining.tracks.model.LicensingRestrictions
+import com.ygorxkharo.trackmining.tracks.model.PlaybackAttribution
+import com.ygorxkharo.trackmining.tracks.model.ProductionAttribution
+import com.ygorxkharo.trackmining.tracks.model.PublishingAttribution
+import com.ygorxkharo.trackmining.tracks.model.SKUs
+import com.ygorxkharo.trackmining.tracks.model.SourcingPlatform
+import com.ygorxkharo.trackmining.tracks.model.TrackContent
+
+/**
+ * Used to convert a Spotify Library track into an Obey Library Track
+ *
+ */
+class LibraryTrackMapper {
+
+    private val platformName = "spotify"
+    private val utcDateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+    private val utcStringFormat = "%s%s"
+
+    /**
+     * This time value was chosen due to a standard in the music industry to release albums by
+     * 00:01 UTC: https://en.wikipedia.org/wiki/Global_Release_Day#:~:text=Music%20releases%20are%20now%20uniformly,and%20singles%20would%20become%20available.
+     * As spotify doesn't offer dates in UTC format, the library track's datetime has to be re-constructed
+     */
+    private val utcTimeTemplate = "T00:01:00.000+0000"
+    private val utcDateFormatter = DateTimeFormatter.ofPattern(utcDateFormat)
+
+    /**
+     * Convert a Spotify library track entity object to a [LibraryTrack] domain object
+     *
+     * @param trackEntity Spotify track object to convert to domain model
+     * @return a domain model representing a library track
+     */
+    fun convertToLibraryTrack(trackEntity: LibraryTrackEntity): LibraryTrack {
+        val trackContent = extractTrackContents(trackEntity)
+        val playbackAttribution = extractPlaybackAttribution(trackEntity)
+        val productionAttribution = extractProductionAttribution(trackEntity)
+        val publishingAttribution = extractPublishingAttribution(trackEntity)
+        val skus = extractSKUs(trackEntity)
+
+        return LibraryTrack(
+            trackContent = trackContent,
+            playbackAttribution = playbackAttribution,
+            productionAttribution = productionAttribution,
+            publishingAttribution = publishingAttribution,
+            skus = skus
+        )
+    }
+
+    /**
+     * Extract the Stock Keeping Units (SkUs) for the library track entity object
+     *
+     * @param trackEntity Contains the SKU data
+     * @return an stock keeping units object containing platform and track recording information
+     */
+    private fun extractSKUs(trackEntity: LibraryTrackEntity): SKUs {
+        return SKUs(
+            isrcCode = trackEntity.externalIds.isrcCode,
+            sourcingPlatform = SourcingPlatform(
+                sourceReferenceSystem = platformName,
+                sourceReferenceId = trackEntity.trackId
+            )
+        )
+    }
+
+    /**
+     * Extract track publishing details from a library track entity
+     *
+     * @param trackEntity Contains publishing attribution details to extract
+     * @return an object containing a release date and publisher/record label information
+     */
+    private fun extractPublishingAttribution(trackEntity: LibraryTrackEntity): PublishingAttribution {
+        val albumReleaseDate = trackEntity.albumContents.releaseDateYYYYmmDD
+        return PublishingAttribution(
+            publisherName = "", // Currently missing from response payload. Will be handled in Jira story B2MVE-1150
+            releaseDateUTC = LocalDateTime.parse(
+                    String.format(utcStringFormat, albumReleaseDate, utcTimeTemplate),
+                    utcDateFormatter
+            )
+        )
+    }
+
+    /**
+     * Extract all the parties involved in the production of the track, based on performance
+     *
+     * @param trackEntity Contains performing artists information to extract. It is assumed that the
+     * first artist on the list is the lead artist
+     * @return an object containing the lead artist, as well as a collection of featured artists
+     */
+    private fun extractProductionAttribution(trackEntity: LibraryTrackEntity): ProductionAttribution {
+        val leadArtist = trackEntity.artists.first()
+        return ProductionAttribution(
+            leadArtist = leadArtist.name,
+            featuredArtists = trackEntity.artists
+                .filter { artist -> artist.name != leadArtist.name }
+                .map { artist -> artist.name }
+        )
+    }
+
+    /**
+     * Extract information on track playback/streaming
+     *
+     * @param trackEntity contains track streaming details
+     * @return an object containing streaming platform name, streaming URL, and regional availability
+     */
+    private fun extractPlaybackAttribution(trackEntity: LibraryTrackEntity): PlaybackAttribution {
+        return PlaybackAttribution(
+            platform = platformName,
+            streamingUri = trackEntity.streamingUri,
+            licensingRestrictions = LicensingRestrictions(
+                regionalAvailability = trackEntity.regionalAvailability
+            )
+        )
+    }
+
+    /**
+     * Extract Spotify track details pertaining to album information and track duration
+     *
+     * @param trackEntity contains track  to extract
+     * @return an object containing the track title, originating album, duration, and genres
+     */
+    private fun extractTrackContents(
+        trackEntity: LibraryTrackEntity,
+    ): TrackContent {
+        return TrackContent(
+            songTitle = trackEntity.title,
+            albumSource = AlbumSource(
+                albumTitle = trackEntity.albumContents.title,
+                coverImageUri = trackEntity.albumContents.images.first().imageUrl
+            ),
+            durationInMillis = trackEntity.durationInMillis,
+            genres = listOf() // Currently missing from response payload. Will be handled in Jira story B2MVE-1150
+        )
+    }
+}
---
+++ data/src/test/java/com/ygorxkharo/obey/trackmining/api/spotify/apiclient/mapper/LibraryTrackMapperTest.kt
@@ -0,0 +1,80 @@
+package com.ygorxkharo.obey.trackmining.api.spotify.apiclient.mapper
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import com.ygorxkharo.obey.trackmining.api.spotify.apiclient.model.LibraryTrackEntity
+import com.ygorxkharo.obey.trackmining.sdk.spotify.apiclient.fixtures.SpotifyTrackJSONResultTestFixture
+import com.ygorxkharo.trackmining.tracks.model.LibraryTrack
+
+internal class LibraryTrackMapperTest {
+
+    private val regionalAvailabilityList = listOf("NG","ZA")
+    private val trackId = "6E1ZonB1Fosz99Rg0tUBTi"
+    private val testTrackStreamingUri = "spotify:track:$trackId"
+    private val albumTitle = "Stories"
+    private val trackTitle = albumTitle
+    private val albumArtUrl = "https://i.scdn.co/image/ab67616d0000b27309bd912a6dd8ab6faad57346"
+    private val artistName = "Refs"
+    private val featureArtists = listOf("DJ Mix")
+    private val trackReleaseDate = "2019-06-07T00:01"
+    private val trackDuration = 219475L
+    private val trackISRCCode = "GBKPL1828466"
+    private val libraryTrackMapper = LibraryTrackMapper()
+    private lateinit var libraryTrackEntity: LibraryTrackEntity
+    private lateinit var actualLibraryTrack: LibraryTrack
+
+
+    @BeforeEach
+    fun setup() {
+        //Arrange
+        libraryTrackEntity = SpotifyTrackJSONResultTestFixture.buildSpotifyTrackItem().track
+
+        //Act
+        actualLibraryTrack = libraryTrackMapper.convertToLibraryTrack(libraryTrackEntity)
+    }
+
+    @Test
+    fun `test when mapping a track entity to a library track, expect a library track to be created`() {
+
+        //Assert
+        assertEquals(albumTitle, actualLibraryTrack.trackContent.albumSource.albumTitle)
+        assertEquals(albumArtUrl, actualLibraryTrack.trackContent.albumSource.coverImageUri)
+        assertEquals(trackTitle, actualLibraryTrack.trackContent.songTitle)
+        assertEquals(trackDuration, actualLibraryTrack.trackContent.durationInMillis)
+    }
+
+    @Test
+    fun `test when mapping a track entity to a library track, expect a library track to be contain SKU information`() {
+
+        //Assert
+        assertEquals(trackISRCCode, actualLibraryTrack.skus.isrcCode)
+        assertEquals("spotify", actualLibraryTrack.skus.sourcingPlatform.sourceReferenceSystem)
+        assertEquals(trackId, actualLibraryTrack.skus.sourcingPlatform.sourceReferenceId)
+    }
+
+    @Test
+    fun `test when mapping a track entity to a library track, expect a library track to be contain playback attribution`() {
+
+        //Assert
+        assertEquals(testTrackStreamingUri, actualLibraryTrack.playbackAttribution.streamingUri)
+        assertEquals(regionalAvailabilityList, actualLibraryTrack.playbackAttribution.licensingRestrictions.regionalAvailability)
+        assertEquals("spotify", actualLibraryTrack.playbackAttribution.platform)
+    }
+
+    @Test
+    fun `test when mapping a track entity to a library track, expect a library track to be contain production attribution`() {
+
+        //Assert
+        assertEquals(artistName, actualLibraryTrack.productionAttribution.leadArtist)
+        assertTrue(actualLibraryTrack.productionAttribution.featuredArtists == featureArtists)
+    }
+
+    @Test
+    fun `test when mapping a track entity to a library track, expect a library track to be contain publishing attribution`() {
+
+        //Assert
+        assertEquals(trackReleaseDate, actualLibraryTrack.publishingAttribution.releaseDateUTC.toString())
+    }
+}
