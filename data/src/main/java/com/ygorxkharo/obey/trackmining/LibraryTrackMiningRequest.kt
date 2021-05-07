09d7d1002dc9491375ef8884b82c23ea3be21c9b
Robert Anizoba <robert@bleeptv.co>
2021-05-07 16:11:28 +0100

    B2MVE-1162 - Create custom exception for track mining process, create object for track mining request

---
+++ data/src/main/java/com/ygorxkharo/obey/trackmining/LibraryTrackMiningRequest.kt
@@ -0,0 +1,8 @@
+package com.ygorxkharo.obey.trackmining
+
+/**
+ * Used to make a request to mine for library tracks
+ *
+ * @property chosenPlatformName The name of the platform to mine from
+ */
+data class LibraryTrackMiningRequest(val chosenPlatformName: String)
---
+++ data/src/main/java/com/ygorxkharo/obey/trackmining/exceptions/LibraryTracksMiningException.kt
@@ -0,0 +1,19 @@
+package com.ygorxkharo.obey.trackmining.exceptions
+
+/**
+ * Represent an error occurring when trying to use a non-existent music
+ * library track miner or something else goes wrong with the mining process
+ */
+class LibraryTracksMiningException: Exception {
+    /**
+     * An exception constructor that takes a message
+     * @param message The message explaining the cause of the error
+     */
+    constructor(message: String): super(message)
+
+    /**
+     * An exception constructor that takes a throwable in the case of chained exceptions
+     * @param throwable The throwable containing the original stacktrace explaining the cause of the error
+     */
+    constructor(throwable: Throwable): super(throwable)
+}
